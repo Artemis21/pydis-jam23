87b845f84fae650998f9dc15708b9368d91e72ea
Shneor <770elmo@gmail.com>
2023-09-01 00:53:08 +0200

    I guess Black dosent like single quotes

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -2,12 +2,12 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    ('a', 'b', 'out'),
+    ("a", "b", "out"),
     [
         (3, 6, 9),
         (1, 2, 3),
         (17, 12, 29),
-    ]
+    ],
 )
 def test_add(a, b, out):
     assert a + b == out
