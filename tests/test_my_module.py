3ac64548b5936dc108e56198cb1bf4de2b03c72d
Shneor <770elmo@gmail.com>
2023-09-01 00:21:59 +0200

    Added test

---
+++ tests/test_my_module.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'out'),
+    [
+        (3, 6, 9),
+        (1, 2, 3),
+        (17, 12, 29),
+        (17, 12, 28),
+
+    ]
+)
+def test_add(a, b, out):
+    assert a + b == out
+
+
+def test_smth():
+    assert True
