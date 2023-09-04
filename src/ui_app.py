60c6907eb191ae6ca2c33660913dec722cd96ddb
solocyberengineer <solocyberengineer@gmail.com>
2023-09-04 21:33:54 +0200

    Updated GUI before adding LSB Module from arty-lsb-stego

--- src/ui_app.py
+++ src/ui_app.py
@@ -8,12 +8,15 @@ class UiApp(QtWidgets.QMainWindow):
         self.ui = uic.loadUi("src/ui_files/main.ui", self)
         # self.ui.setWindowIcon() # we need a icon
         # self.ui.setWindowTitle() # already set in 'main.ui' file but just incase
+        self.lsbframe.hide()
         self._functions()
+        self.statusBar.showMessage("Application Started")
         self.show()  # displays the QMainWindow in QApplication Instance
 
     def _functions(self):
-        self.action_open_image.triggered.connect(lambda: AppFuncs.action_open_image(self.image_viewer))
+        self.action_open_image.triggered.connect(lambda: AppFuncs.action_open_image(self.statusBar, self.image_viewer))
         self.action_save_image.triggered.connect(AppFuncs.action_save_image)
+        self.lsbtoolbtn.clicked.connect(lambda: AppFuncs.lsbtoolsection(self.statusBar, self.lsbframe))
 
     # def resizeEvent(self, event):
     # ^ this is a problem for hatch formatting because this method is inherited from QMainWindow
@@ -26,11 +29,12 @@ class UiApp(QtWidgets.QMainWindow):
 
 class AppFuncs:
     @staticmethod
-    def action_open_image(image_viewer):
+    def action_open_image(status, image_viewer):
         # this function opens a image in the gui app
-        print("opening image")
+        status.showMessage("file dialog opened")
         filename = QtWidgets.QFileDialog.getOpenFileName()
         print(filename)
+        status.showMessage(f"Image: {filename[0]}")
         # dlg = QtWidgets.QFileDialog()
         # dlg.setFileMode(QtWidgets.QFileDialog.AnyFile)
         # dlg.setFilter("Text files (*.*)")
@@ -38,12 +42,22 @@ class AppFuncs:
         # print(filenames)
         # image_viewer to display opened image
         image_viewer.setPixmap(QtGui.QPixmap(filename[0]))
+        status.showMessage(f"{filename[0]} displayed")
         # need to find a zooming solution for the image
 
     @staticmethod
     def action_save_image():
         # this function saves a image in the gui app
         print("saving image")
+
+    @staticmethod
+    def lsbtoolsection(status, _frame):
+        if _frame.isHidden():
+            _frame.show()
+            status.showMessage("lsbtools opened")
+        elif not _frame.isHidden():
+            _frame.hide()
+            status.showMessage("lsbtools closed")
 
 
 def run_app():
