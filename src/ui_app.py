fd92a0b3cbe50867b6dbc3cf85f7c5ac8e84da9d
solocyberengineer <solocyberengineer@gmail.com>
2023-09-04 16:09:29 +0200

    Added some more ui. You can now open your own image.

--- src/ui_app.py
+++ src/ui_app.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui, QtWidgets, uic
 
 
 class UiApp(QtWidgets.QMainWindow):
@@ -9,10 +9,10 @@ class UiApp(QtWidgets.QMainWindow):
         # self.ui.setWindowIcon() # we need a icon
         # self.ui.setWindowTitle() # already set in 'main.ui' file but just incase
         self._functions()
-        self.show()
+        self.show()  # displays the QMainWindow in QApplication Instance
 
     def _functions(self):
-        self.action_open_image.triggered.connect(AppFuncs.action_open_image)
+        self.action_open_image.triggered.connect(lambda: AppFuncs.action_open_image(self.image_viewer))
         self.action_save_image.triggered.connect(AppFuncs.action_save_image)
 
     # def resizeEvent(self, event):
@@ -25,11 +25,22 @@ class UiApp(QtWidgets.QMainWindow):
 
 
 class AppFuncs:
-    def action_open_image():
+    @staticmethod
+    def action_open_image(image_viewer):
         # this function opens a image in the gui app
         print("opening image")
-        # self.image_viewer to display opened image
+        filename = QtWidgets.QFileDialog.getOpenFileName()
+        print(filename)
+        # dlg = QtWidgets.QFileDialog()
+        # dlg.setFileMode(QtWidgets.QFileDialog.AnyFile)
+        # dlg.setFilter("Text files (*.*)")
+        # filenames = QtCore.QStringListModel()
+        # print(filenames)
+        # image_viewer to display opened image
+        image_viewer.setPixmap(QtGui.QPixmap(filename[0]))
+        # need to find a zooming solution for the image
 
+    @staticmethod
     def action_save_image():
         # this function saves a image in the gui app
         print("saving image")
