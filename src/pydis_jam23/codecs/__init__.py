99201234ea26836a946072ac7e514665db5ba1a6
MrBud <90530825+solocyberengineer@users.noreply.github.com>
2023-09-09 17:45:37 +0200

    Add GUI frontend
    * This is the **base layout code** for gui in PyQt5.<br> > Added PyQt5 to pyproject.toml dependancies.<br> > Added seperate module 'src\ui_app.py' for the gui and its functionality.<br> > The ui_files folder consists of the ui 'main.ui' used by 'ui_app.py' module to display what it currently has.<br> Just posting this so that everyone could have idea of PyQt's workings.
    
    * Added some more ui. You can now open your own image.
    
    * Updated GUI before adding LSB Module from arty-lsb-stego
    
    * Updated 'ui_app.py'
    
    * removed coded comments
    
    * Separate GUI and CLI logic from core codec logic
    
    * Remove accidentally committed images
    
    ---------
    
    Co-authored-by: Artemis <me@arty.li>

--- src/pydis_jam23/codecs/__init__.py
+++ src/pydis_jam23/codecs/__init__.py
@@ -5,8 +5,6 @@ from PIL import Image
 from . import lsb
 from .common import CodecError
 
-__all__ = ["CodecError", "CODECS", "Codec"]
-
 
 class Codec(Protocol):
     cli_flag: str
@@ -20,3 +18,5 @@ class Codec(Protocol):
 
 
 CODECS: list[Codec] = [lsb]
+
+__all__ = ["CodecError", "CODECS", "Codec"]
